d7b9ce2203604bb26c3289b51ca54024b7599486
bmomofoma <mikeerune08@gmail.com>
2026-06-23 15:31:44 -0700

    Update pc_reg.sv

--- RTL/core/pc_reg.sv
+++ RTL/core/pc_reg.sv
@@ -7,8 +7,7 @@ module pc_reg (
     output logic [31:0] pc
 );
 
-    // Updates the pointer on every clock cycle.
-    // On rst, we force it back to 0 to restart the program execution.
+    
     always_ff @(posedge clk) begin
         if (rst)
             pc <= 32'h0000_0000;
