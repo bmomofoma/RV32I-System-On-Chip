5e6f1984abfa5911ac339b499d20e5af15fcb3b5
bmomofoma <mikeerune08@gmail.com>
2026-06-23 15:22:50 -0700

    Update soc_tb.sv

--- Test_bench/soc_tb.sv
+++ Test_bench/soc_tb.sv
@@ -1,14 +1,12 @@
 `timescale 1ns / 1ps
 
 module soc_tb;
-
-    // Testbench signals
     logic clk;
     logic rst;
     wire  sda;
     wire  scl;
 
-    // Instantiate the Top-Level SoC
+    // the Top-Level SoC
     soc_top dut (
         .clk(clk),
         .rst(rst),
@@ -16,7 +14,7 @@ module soc_tb;
         .scl(scl)
     );
 
-    // 100 MHz Clock Generation (Matches Basys 3)
+    // 100 MHz Clock Generation 
     initial begin
         clk = 0;
         forever #5 clk = ~clk; 
@@ -30,7 +28,7 @@ module soc_tb;
         #20;
         rst = 0;
         
-        // Let the CPU fetch and execute the program for 100 clock cycles
+        // the CPU fetch and execute the program for 100 clock cycles
         #1000;
         
         $display("Simulation Complete.");
